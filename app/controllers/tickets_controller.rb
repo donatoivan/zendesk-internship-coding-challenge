395c36d5d42c4dc53088250775afe7971c7d016d
Ivan Donato <donato.ivan@gmail.com>
2019-06-06 00:05:05 +1000

    parsed api response

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,6 +4,7 @@ class TicketsController < ApplicationController
       :password => Rails.application.credentials.zendesk_api[:password]}
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?per_page=25",
                       :basic_auth => @auth)
+    @tickets = @response.parsed_response["tickets"]
   end
 
   def show
