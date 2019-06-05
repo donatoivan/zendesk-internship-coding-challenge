8a69e3c958f9fb4c1dc851e25142619868f7fe68
Ivan Donato <donato.ivan@gmail.com>
2019-06-05 23:59:01 +1000

    updated index action in tickets controller, using url for 25 tickets per page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,9 @@
 class TicketsController < ApplicationController
   def index
+    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
+      :password => Rails.application.credentials.zendesk_api[:password]}
+    @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?per_page=25",
+                      :basic_auth => @auth)
   end
 
   def show
