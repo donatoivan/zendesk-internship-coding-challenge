7ae060a359f4731fd749bd96226576837a7996a3
Ivan Donato <donato.ivan@gmail.com>
2019-06-06 01:04:42 +1000

    refactored tickets_controller code

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,24 +1,26 @@
 class TicketsController < ApplicationController
   def paginate
-    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
-              :password => Rails.application.credentials.zendesk_api[:password]}
     @response_for_page = params[:id].to_i
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?page=#{@response_for_page}&per_page=25", 
-                      :basic_auth => @auth)
-    if (@response.parsed_response['tickets'] == nil || @response.parsed_response["error"] == "Couldn't authenticate you")
-      render :error
-    else
-      @tickets = @response.parsed_response["tickets"]
-    end
+                      :basic_auth => credentials)
+    check_error(@response) ? (render :error) : @tickets = @response.parsed_response["tickets"]
   end
 
   def show
-    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
-              :password => Rails.application.credentials.zendesk_api[:password]}
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets/#{params[:id]}.json", 
-                      :basic_auth => @auth)
-    @ticket = @response.parsed_response["ticket"]
+                      :basic_auth => credentials)
+    check_error(@response) ? (render :error) : @ticket = @response.parsed_response["ticket"]
   end
 
+  private
+  def credentials
+    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
+      :password => Rails.application.credentials.zendesk_api[:password]}
+  end
 
+  def check_error(response)
+    if (response.code == 404 || @response.code == 401)
+      true
+    end   
+  end
 end
