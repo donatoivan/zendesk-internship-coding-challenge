6a09612b2e0a2d7ba6157aa366c1611bd8c7a7a6
Ivan Donato <donato.ivan@gmail.com>
2019-06-05 23:54:49 +1000

    added index, show and paginate actions to tickets controller, custom paginate route

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,10 @@
 class TicketsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def paginate
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tickets#index'
   resources :tickets
+
+  get '/tickets/pages/:id', to: 'tickets#paginate', as: 'paginate'
 end
