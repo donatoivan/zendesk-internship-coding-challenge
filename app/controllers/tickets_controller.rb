fb903423f1ea7236e65f27334147306a01045bbb
Ivan Donato <donato.ivan@gmail.com>
2019-06-06 00:13:46 +1000

    updated paginate action, paginate tag in index

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,19 +1,26 @@
 class TicketsController < ApplicationController
   def index
     @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
-      :password => Rails.application.credentials.zendesk_api[:password]}
+              :password => Rails.application.credentials.zendesk_api[:password]}
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?per_page=25",
                       :basic_auth => @auth)
     @tickets = @response.parsed_response["tickets"]
   end
 
   def show
-    @auth = {:username => "donato.ivan@gmail.com", :password => "Zendeskcodingchallenge"}
+    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
+              :password => Rails.application.credentials.zendesk_api[:password]}
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets/#{params[:id]}.json", 
                       :basic_auth => @auth)
     @ticket = @response.parsed_response["ticket"]
   end
 
   def paginate
+    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
+              :password => Rails.application.credentials.zendesk_api[:password]}
+    @response_for_page = params[:id].to_i
+    @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?page=#{@response_for_page}&per_page=25", 
+                      :basic_auth => @auth)
+    @tickets = @response.parsed_response["tickets"]
   end
 end
