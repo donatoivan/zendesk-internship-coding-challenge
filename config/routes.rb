e9f958bfc4dbdc94ec14bcfdb67f7b7094517b6f
Ivan Donato <donato.ivan@gmail.com>
2019-06-07 12:36:38 +1000

    added ticket-nav partial, updated pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tickets, only: [:index, :show]
+  resources :tickets, only: [:show]
 
   get '/tickets/pages/:id', to: 'tickets#paginate', as: 'paginate'
   root 'home#index'
