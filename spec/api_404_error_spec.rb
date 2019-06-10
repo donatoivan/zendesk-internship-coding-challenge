dbbbf6225309d0c64fee793daafd2b1561ad91e3
Ivan Donato <donato.ivan@gmail.com>
2019-06-11 00:44:45 +1000

    refactored error tests

--- spec/api_404_error_spec.rb
+++ spec/api_404_error_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
+require 'support/response_helper'
 require 'httparty'
 
 describe "API 404 error testing" , :type => :api do
+  include ResponseHelper
   before do
+    @url = "https://donatooivan.zendesk.com/api/v2/tickets.json?"
+    @title_msg = "No help desk at donatooivan.zendesk.com"
+    @msg_body = "There is no help desk configured at this address. This means that the address is available and that you can claim it at http://www.zendesk.com/signup"
+
+
     json_response = File.open("./test/fixtures/404.json")
     stub_request(:get, "https://donatooivan.zendesk.com/api/v2/tickets.json?").
       with(basic_auth: [Rails.application.credentials.zendesk_api[:username], Rails.application.credentials.zendesk_api[:password] ]).
@@ -10,24 +17,11 @@ describe "API 404 error testing" , :type => :api do
     end
       
     it 'returns a 404 code' do
-      url = "https://donatooivan.zendesk.com/api/v2/tickets.json?"
-  
-      response = HTTParty.get(url,
-        :basic_auth => {:username => Rails.application.credentials.zendesk_api[:username], 
-          :password => Rails.application.credentials.zendesk_api[:password]})
-      expect(response.code).to eql(404)
+      expect(response_no_json(@url).code).to eql(404)
     end
 
     it 'returns appropriate error body' do
-      url = "https://donatooivan.zendesk.com/api/v2/tickets.json?"
-
-      title_msg = "No help desk at donatooivan.zendesk.com"
-      msg_body = "There is no help desk configured at this address. This means that the address is available and that you can claim it at http://www.zendesk.com/signup"
-  
-      response = JSON.load(HTTParty.get(url,
-        :basic_auth => {:username => Rails.application.credentials.zendesk_api[:username], 
-          :password => Rails.application.credentials.zendesk_api[:password]}))
-      expect(response["error"]["title"]).to eql(title_msg)
-      expect(response["error"]["message"]).to eql(msg_body)
+      expect(response_helper(@url)["error"]["title"]).to eql(@title_msg)
+      expect(response_helper(@url)["error"]["message"]).to eql(@msg_body)
     end
 end   
