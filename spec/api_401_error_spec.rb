e302c20823fac0f2239240e7f259143c2c027cfe
Ivan Donato <donato.ivan@gmail.com>
2019-06-11 16:10:53 +1000

    refactored code to remove database

---
+++ spec/api_401_error_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'support/response_helper'
+require 'httparty'
+
+describe "API 404 error testing" , :type => :api do
+  include ResponseHelper
+  before do
+    @url = "https://donatoivan.zendesk.com/api/v2/tickets.json?"
+    @error_msg = "Couldn't authenticate you"
+    json_response = File.open("./test/fixtures/401.json")
+    stub_request(:get, "https://donatoivan.zendesk.com/api/v2/tickets.json?").
+      to_return(status: 401, body: json_response)
+    end
+      
+    it 'returns a 401 code' do
+      response = HTTParty.get(@url)
+      expect(response.code).to eql(401)
+    end
+
+    it 'returns appropriate error body' do
+      expect(response_helper(@url)["error"]).to eql(@error_msg)
+    end
+end   
---
+++ spec/features/custom_error_routes_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'custom not found page' do
+  it 'should respond with 404 page' do
+    content_404 = "zendesk\nPage Not Found\nOops! We all make mistakes\nThis page may have moved or you might have typed something incorrectly\nBack to Tickets\n404"
+    visit '/ticketasdasasdasd'
+    expect(page).to have_content(content_404)
+  end
+end
+
+describe 'custom internal error page' do 
+  it 'should respond with 500 page' do
+    content_500 = "We're sorry, but something went wrong.\nIf you are the application owner check the logs for more information."
+    visit "/500"
+    expect(page).to have_content(content_500)
+  end
+end
