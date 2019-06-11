4b70801553e4028778335dc4acaee898b1661142
Ivan Donato <donato.ivan@gmail.com>
2019-06-11 16:01:23 +1000

    removed most files due to database removal

--- app/models/ticket.rb
+++
@@ -1,3 +0,0 @@
-class Ticket < ApplicationRecord
-  include HTTParty
-end
--- db/migrate/20190605135130_create_tickets.rb
+++
@@ -1,8 +0,0 @@
-class CreateTickets < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tickets do |t|
-
-      t.timestamps
-    end
-  end
-end
