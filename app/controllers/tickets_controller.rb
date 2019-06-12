dd431f3030aa8359bfb24e32fa9ad33642f3e1d4
Ivan Donato <donato.ivan@gmail.com>
2019-06-12 13:59:06 +1000

    refactored controllers

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,28 +3,28 @@ class TicketsController < ApplicationController
   def paginate
     @page_number = params[:id].to_i
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?page=#{@page_number}&per_page=25",
-                      :basic_auth => credentials)
-    if @response.parsed_response["tickets"].empty?
+                      basic_auth: credentials)
+    if @response.parsed_response['tickets'].empty?
       raise ActionController::RoutingError.new('not_found')
     else 
-      check_error(@response) ? (render :error) : @tickets = @response.parsed_response["tickets"]
+      check_error(@response) ? (render :error) : @tickets = @response.parsed_response['tickets']
     end
   end
 
   def show
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets/#{params[:id]}.json", 
-                      :basic_auth => credentials)
-    if @response.parsed_response["error"] == "RecordNotFound"
+                      basic_auth: credentials)
+    if @response.parsed_response['error'] == 'RecordNotFound'
       raise ActionController::RoutingError.new('not_found')
     else
-      check_error(@response) ? (render :error) : @ticket = @response.parsed_response["ticket"]
+      check_error(@response) ? (render :error) : @ticket = @response.parsed_response['ticket']
     end
   end
 
   private
   def credentials
-    @auth = {:username => Rails.application.credentials.zendesk_api[:username], 
-      :password => Rails.application.credentials.zendesk_api[:password]}
+    @auth = {username: Rails.application.credentials.zendesk_api[:username], 
+      password: Rails.application.credentials.zendesk_api[:password]}
   end
 
   def check_error(response)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :tickets, only: :show
 
   match '/404', to: 'errors#not_found', via: :all
-  match "/500", to: 'errors#internal_server_error', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
   
   get '/tickets/pages/:id', to: 'tickets#paginate', as: 'paginate'
   root 'home#index'
