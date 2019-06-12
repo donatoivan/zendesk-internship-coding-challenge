f4bf299495c4a96b4a4d2159af8dc0f2b4169311
Ivan Donato <donato.ivan@gmail.com>
2019-06-12 14:57:00 +1000

    removed unneeded commented code

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,22 +1,10 @@
 class TicketsController < ApplicationController
   include HTTParty
+  
   def paginate
     @page_number = params[:id].to_i
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets.json?page=#{@page_number}&per_page=25",
                       basic_auth: credentials)
-
-    # check_error(@response) ? (render :error) : @tickets = @response.parsed_response['tickets']
-    # if @tickets.empty?
-    #   raise ActionController::RoutingError.new('not_found')
-    # end
-
-    # if check_error(@response) 
-    #   render :error
-    # elsif check_error(@response) != true 
-    #   @tickets = @response.parsed_response['tickets']
-    # else (check_error(@response) != true) && (@response['tickets'].empty? == true)
-    #   raise ActionController::RoutingError.new('not_found')
-    # end
 
     if (check_error(@response) != true) && (@response['tickets'].empty? == true)
       raise ActionController::RoutingError.new('not_found')
@@ -30,7 +18,9 @@ class TicketsController < ApplicationController
   def show
     @response = HTTParty.get("https://donatoivan.zendesk.com/api/v2/tickets/#{params[:id]}.json", 
                       basic_auth: credentials)
+
     check_error(@response) ? (render :error) : @ticket = @response.parsed_response['ticket']
+    
     if @response.parsed_response['error'] == 'RecordNotFound'
       raise ActionController::RoutingError.new('not_found')
     end
