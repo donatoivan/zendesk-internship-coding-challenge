3c21425bdd37852bba20002e909542c805082fdd
Ivan Donato <donato.ivan@gmail.com>
2019-06-12 13:54:25 +1000

    added error handling for tickets beyond array limit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tickets, only: [:show]
+  resources :tickets, only: :show
 
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
+  match '/404', to: 'errors#not_found', via: :all
+  match "/500", to: 'errors#internal_server_error', via: :all
   
   get '/tickets/pages/:id', to: 'tickets#paginate', as: 'paginate'
   root 'home#index'
